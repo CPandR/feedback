d39ba547e0ffe7d7c1ca6aa091d1215c67d246ab
ruairidhflint <roryflintphoto@gmail.com>
2021-08-18 15:24:24 +0100

    Add error page

---
+++ src/pages/Error/index.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import Error from './index';
+
+test('renders Error component with correct error message - Invalid ID', () => {
+  render(<Error error="Invalid ID" />);
+  const title = screen.getByText(/Uh oh!/i);
+  const details = screen.getByText(/It looks like you've taken a wrong turn./i);
+  expect(title).toBeInTheDocument();
+  expect(details).toBeInTheDocument();
+});
+
+test('renders Error component with correct error message - Booking not complete', () => {
+  render(<Error error="Booking not complete" />);
+  const title = screen.getByText(/Uh oh!/i);
+  const detail = screen.getByText(
+    /It looks like the session you are trying to leave feedback for has not yet been marked as complete/i,
+  );
+  expect(title).toBeInTheDocument();
+  expect(detail).toBeInTheDocument();
+});
+
+test('renders Error component with correct error message - Feedback not required', () => {
+  render(<Error error="Feedback not required" />);
+  const title = screen.getByText(/Uh oh!/i);
+  const detail = screen.getByText(
+    /It looks like the session you are trying to leave feedback for does not require it/i,
+  );
+  expect(title).toBeInTheDocument();
+  expect(detail).toBeInTheDocument();
+});
+
+test('renders Error component with correct error message - Feedback already submitted', () => {
+  render(<Error error="Feedback already submitted" />);
+  const title = screen.getByText(/Uh oh!/i);
+  const detail = screen.getByText(/It looks like feedback has already been submitted/i);
+  expect(title).toBeInTheDocument();
+  expect(detail).toBeInTheDocument();
+});
--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -3,6 +3,26 @@ interface ErrorProps {
 }
 
 const Error = ({ error }: ErrorProps) => {
-  return <div className="error-container">Error!</div>;
+  let message;
+  if (error === 'Booking not complete') {
+    message =
+      'It looks like the session you are trying to leave feedback for has not yet been marked as complete. Please try again shortly or contact your Cardiac Coach if the problen persists.';
+  } else if (error === 'Feedback not required') {
+    message =
+      'It looks like the session you are trying to leave feedback for does not require it. If you have followed a link from an email or believe this is incorrect please contact your Cardiac Coach.';
+  } else if (error === 'Feedback already submitted') {
+    message =
+      'It looks like feedback has already been submitted for this session! If you believe this to be a mistake, please contact your Cardiac Coach.';
+  } else {
+    message =
+      "It looks like you've taken a wrong turn. If you were expecting to submit feedback, please try following the link sent via email again or contact your Cardiac Coach.";
+  }
+
+  return (
+    <div className="error-container">
+      <h1>Uh oh!</h1>
+      <p>{message}</p>
+    </div>
+  );
 };
 export default Error;
