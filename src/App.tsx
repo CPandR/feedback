be54f787bf0e140c0ed9f183feff9e6e80393b25
ruairidhflint <roryflintphoto@gmail.com>
2021-08-19 07:35:29 +0100

    Update copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,18 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import qs, { ParsedQuery } from 'query-string';
 import './App.css';
 import FeedbackForm from './pages/FeedbackForm';
 import Loading from './pages/Loading';
 import Error from './pages/Error';
 import Success from './pages/Success';
-import prod from './config/config';
+import { Route } from 'react-router-dom';
 
 function App() {
-  const [state, setState] = useState<string>('loading');
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const { id }: ParsedQuery<string> = qs.parse(window.location.search);
-    axios
-      .get(`${prod}/validate_feedback/${id}`)
-      .then(() => {
-        setState('form');
-      })
-      .catch((err) => {
-        const { error } = err?.response?.data;
-        setState('error');
-        setError(error);
-      });
-  }, []);
-
-  // Initially loading
-  if (state === 'loading') {
-    return <Loading />;
-  }
-  // Error state
-  if (state === 'error') {
-    return <Error error={error} />;
-  }
-
-  // Form state
-  if (state === 'form') {
-    return <FeedbackForm setState={setState} />;
-  }
-
-  // Success state
-  if (state === 'success') {
-    return <Success />;
-  }
-
-  // Fallback error state
-  else {
-    return <Error error={error} />;
-  }
+  return (
+    <>
+      <Route exact path="/" component={FeedbackForm} />
+      <Route path="/loading" component={Loading} />
+      <Route path="/error" component={Error} />
+      <Route path="/success" component={Success} />
+    </>
+  );
 }
 export default App;
--- src/pages/FeedbackForm/index.tsx
+++ src/pages/FeedbackForm/index.tsx
@@ -1,13 +1,6 @@
 import { useState } from 'react';
-import axios from 'axios';
-import qs, { ParsedQuery } from 'query-string';
-import prod from '../../config/config';
 import Question from '../../components/Question';
 import Button from '../../components/Button';
-
-interface FeedbackFormProps {
-  setState: Function;
-}
 
 interface IFeedback {
   1: number | null;
@@ -16,31 +9,20 @@ interface IFeedback {
   4: number | null;
 }
 
-function FeedbackForm({ setState }: FeedbackFormProps) {
+function FeedbackForm(props: any) {
   const [feedback, setFeedback] = useState<IFeedback>({ 1: null, 2: null, 3: null, 4: null });
 
   const submitFeedback = () => {
-    const { id }: ParsedQuery<string> = qs.parse(window.location.search);
-    axios
-      .patch(`${prod}/submit_feedback/${id}`, {
-        coaching: feedback['1'],
-        project_management: feedback['2'],
-        four_pillars: feedback['3'],
-        communication: feedback['4'],
-      })
-      .then(() => {
-        setState('success');
-      });
+    props.history.push('/success');
   };
 
   return (
     <div className="container">
       <h1>We'd love to hear your feedback</h1>
       <p className="context">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab velit eaque, quasi rem incidunt quidem rerum
-        eligendi, corporis voluptas iusto repellendus molestias perspiciatis omnis sit! Soluta architecto nostrum
-        tenetur necessitatibus! iusto repellendus molestias perspiciatis omnis sit! Soluta architecto nostrum tenetur
-        necessitatibus!
+        For each of the four areas below, please choose the value that you believe best corresponds to your experience
+        at CP+R. A score of 1 would indicate that you strongly disagree, 2 that you disagree, 3 that you neither agree
+        or disagree, 4 that you agree and finally a score of 5 would indicate that you strongly agree.
       </p>
       <Question
         title="Coaching & Session Quality"
