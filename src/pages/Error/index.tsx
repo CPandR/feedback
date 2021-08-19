5836d3e6e4382bf54d14ce5e34baf17f746c1525
ruairidhflint <roryflintphoto@gmail.com>
2021-08-19 07:21:06 +0100

    Add dynamic title

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -4,6 +4,7 @@ interface ErrorProps {
 
 const Error = ({ error }: ErrorProps) => {
   let message;
+  let title = 'Uh oh!';
   if (error === 'Booking not complete') {
     message =
       'It looks like the session you are trying to leave feedback for has not yet been marked as complete. Please try again shortly or contact your Cardiac Coach if the problen persists.';
@@ -13,6 +14,7 @@ const Error = ({ error }: ErrorProps) => {
   } else if (error === 'Feedback already submitted') {
     message =
       'It looks like feedback has already been submitted for this session! If you believe this to be a mistake, please contact your Cardiac Coach.';
+    title = 'Thank You!';
   } else {
     message =
       "It looks like you've taken a wrong turn. If you were expecting to submit feedback, please try following the link sent via email again or contact your Cardiac Coach.";
@@ -20,7 +22,7 @@ const Error = ({ error }: ErrorProps) => {
 
   return (
     <div className="alt-container">
-      <h1>Uh oh!</h1>
+      <h1>{title}</h1>
       <p>{message}</p>
     </div>
   );
