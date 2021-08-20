f90f58b3fe3d35d881a4dd7048e6380bbb7c1e52
ruairidhflint <roryflintphoto@gmail.com>
2021-08-20 08:09:17 +0100

    Add accessibility selection

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,3 +1,5 @@
+import { KeyboardEvent } from 'react';
+
 interface QuestionProps {
   title: string;
   detail: string;
@@ -10,6 +12,12 @@ export const Question = ({ title, detail, question, currentValue, setFeedback }:
   const handleChange = (val: Number) => {
     setFeedback((prev: object) => ({ ...prev, [question]: val }));
   };
+
+  const handleKeyPress = (e: KeyboardEvent, num: number) => {
+    if (e.key === 'Enter') {
+      handleChange(num);
+    }
+  };
   return (
     <div className="question-section">
       <h2>{title}</h2>
@@ -18,10 +26,12 @@ export const Question = ({ title, detail, question, currentValue, setFeedback }:
         {Array.from({ length: 5 }, (_, i) => 1 + i * 1).map((num) => {
           return (
             <div
+              tabIndex={question * 5 + num}
               key={`${title} - ${num}`}
               role="button"
               className={`number ${currentValue === num ? 'selected' : ''}`}
               onClick={() => handleChange(num)}
+              onKeyPress={(e) => handleKeyPress(e, num)}
             >
               {num}
             </div>
