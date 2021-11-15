a3ecd1531f5a879c753597d836325894692946ac
ruairidhflint <roryflintphoto@gmail.com>
2021-11-15 10:52:27 +0000

    Update endpoint prod

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 const environment: string = process.env.NODE_ENV;
 const prod: string =
-  environment === 'production' ? 'https://thehub.cpandr.co.uk/api/booking' : 'http://localhost:5001/api/booking';
+  environment === 'production' ? 'https://thehub.cpandr.co.uk/api/feedback' : 'http://localhost:5001/api/feedback';
 export default prod;
